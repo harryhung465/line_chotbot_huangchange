95c0cd2096e9ac5754943a2c804913cc670d4fda
harry <harry@localhost.localdomain>
2019-11-10 17:09:20 +0800

    new file:   ID_message/user_ID_eventt.json 	new file:   check_url.sh 	new file:   text.ipynb

---
+++ check_url.sh
@@ -0,0 +1,6 @@
+#bin/bash
+sudo docker-compose up -d
+sleep 5s
+sudo curl $(docker port ngrok 4040)/api/tunnels > tunnels.json
+sudo docker run -v $(pwd)/tunnels.json:/tmp/tunnels.json --rm  realguess/jq jq .tunnels[1].public_url /tmp/tunnels.json
+sudo docker exec -it jupyter bash
